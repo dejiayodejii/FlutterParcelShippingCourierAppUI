0bddbca7cb7ff448afaf02b0ac2ea9991722cd01
dejiayodejii <dejiayodeji010>
2022-02-22 17:36:02 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:parcel_delivery_app/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Delivery App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
